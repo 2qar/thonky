f7a3926dc920b8c1aa030ed18e5d2c79dd4d777d
tuckerrrrrrrrrrrr <tucker1014@hotmail.com>
2018-11-09 15:32:31 -0800

    small config key fix

--- bot/server_info.py
+++ bot/server_info.py
@@ -21,7 +21,7 @@ class ServerInfo:
 
     def get_ping_channel(self):
         with DBHandler() as handler:
-            channel_id = handler.get_server_config()['announce_channel_id']
+            channel_id = handler.get_server_config()['announce_channel']
             try:
                 return self.bot.get_channel(channel_id)
             except NotFound:
