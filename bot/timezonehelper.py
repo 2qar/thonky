099f712c2c0df64663653123e4744469abe20324
tuckerrrrrrrrrrrr <tucker1014@hotmail.com>
2018-11-23 19:21:53 -0800

    merged get_start_time and get_start_time_info, just returns hour now

--- bot/timezonehelper.py
+++ bot/timezonehelper.py
@@ -23,19 +23,14 @@ def get_timezone(tz: str):
 def get_start_time(tz: str):
     zone = get_timezone(tz)
     if zone:
-        return get_start_time_info(zone)[0]
+        utc_now = datetime.datetime.utcnow()
+        utc_start = datetime.datetime(utc_now.year, utc_now.month, utc_now.day, 23, 0, 0, tzinfo=utc)
+        localized = utc_start.astimezone(tz)
+        if localized:
+            if not is_dst(localized):
+                localized += datetime.timedelta(hours=1)
+            return localized.hour % 12
 
 
 def is_dst(date: datetime):
     return bool(date.dst())
-
-
-def get_start_time_info(tz: timezone):
-    utc_now = datetime.datetime.utcnow()
-    # starting time = 4 PM PDT
-    utc_start = datetime.datetime(utc_now.year, utc_now.month, utc_now.day, 23, 0, 0, tzinfo=utc)
-    localized = utc_start.astimezone(tz)
-    if localized:
-        if not is_dst(localized):
-            localized += datetime.timedelta(hours=1)
-        return [localized.hour % 12, utc_start.tzname()]
