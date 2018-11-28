da56aafecd029aaba16a5b472fa4e9fea2411c7d
tucker <tucker1014@hotmail.com>
2018-11-28 08:43:32 -0800

    bot.py pep8, organization stuff

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,5 @@
 from discord.ext.commands import Bot as DiscordBot
 from discord import Game
-import asyncio
 import importlib
 import os
 from typing import Union
@@ -8,18 +7,18 @@ from typing import Union
 from .server_info import ServerInfo
 from .dbhandler import DBHandler
 
-#TODO: Rewrite sheetbot and odscraper as modules with their own formatters
+# TODO: Rewrite sheetbot and odscraper as modules with their own formatters
 '''
 bot
-	modules
-		odscraper
-			odformatter
-			functionality stuff
-		sheetbot
-			sheet_formatter
-			functionality stuff
+    modules
+        odscraper
+            odformatter
+            functionality stuff
+        sheetbot
+            sheet_formatter
+            functionality stuff
 
-	Pass the bot and channel to the formatter and the formatter constructs embeds AND sends the message
+    Pass the bot and channel to the formatter and the formatter constructs embeds AND sends the message
 '''
 
 # TODO: Maybe write spreadsheet info to the disk so the sheets dont have to get scanned every time the bot is booted
@@ -28,6 +27,7 @@ bot
 class Bot(DiscordBot):
     def __init__(self, token):
         super().__init__('!')
+        self.server_info = {}
         self.run(token)
 
     def add_cogs(self):
@@ -35,17 +35,17 @@ class Bot(DiscordBot):
             if os.path.exists(f'bot/cogs/{cog}/cog.py'):
                 module = importlib.import_module(f'.cogs.{cog}.cog', package='bot')
                 getattr(module, 'setup')(self)
-    
-    async def on_ready(self):
-        playing = Game("with spreadsheets")
-        await self.change_presence(activity=playing)
 
-        self.add_cogs()
-
-        self.server_info = {}
+    def get_server_info(self):
         with DBHandler() as handler:
             for guild in self.guilds:
                 self.create_guild_info(guild.id, handler=handler)
+
+    async def on_ready(self):
+        playing = Game("with spreadsheets")
+        await self.change_presence(activity=playing)
+        self.add_cogs()
+        self.get_server_info()
 
         print("Ready! :)")
             
