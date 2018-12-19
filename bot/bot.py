90b3bb4b0c459c4cde26464055825bf68a32d82d
tuckerrrrrrrrrrrr <tucker1014@hotmail.com>
2018-12-19 14:02:16 -0800

    cleaned up getting server info on bot startup

--- bot/bot.py
+++ bot/bot.py
@@ -39,7 +39,7 @@ class Bot(DiscordBot):
     def get_server_info(self):
         with DBHandler() as handler:
             for guild in self.guilds:
-                self.create_guild_info(guild.id, handler=handler)
+                self.create_guild_info(guild.id, handler)
 
     async def on_ready(self):
         playing = Game("with spreadsheets")
@@ -52,32 +52,19 @@ class Bot(DiscordBot):
     async def on_guild_join(self, guild):
         await self.wait_until_ready()
 
-        self.create_guild_info(guild.id)
+        with DBHandler() as handler:
+            self.create_guild_info(guild.id, handler)
         # send a message that's like "hey admins you should !set_sheet and !set_team"
         # also mention the default settings
         # also mention setting the ping channel
 
-    @staticmethod
-    def create_guild_config(guild_id: Union[str, int], handler=None):
-        print(f"Creating config for server with ID [{guild_id}]")
+    def create_guild_info(self, guild_id: Union[str, int], handler):
+        config = handler.get_server_config(guild_id)
 
-        if handler:
-            if not handler.get_server_config(guild_id):
-                handler.add_server_config(guild_id)
-        else:
-            with DBHandler() as handler:
-                if not handler.get_server_config(guild_id):
-                    handler.add_server_config(guild_id)
-
-    def create_guild_info(self, guild_id: Union[str, int], handler=None):
-        if handler:
-            config = handler.get_server_config(guild_id)
-        else:
-            with DBHandler() as handler:
-                config = handler.get_server_config(guild_id)
         if config:
             doc_key = config['doc_key']
             if doc_key:
                 self.server_info[str(guild_id)] = ServerInfo(guild_id, config, self)
         else:
-            Bot.create_guild_config(guild_id, handler=handler)
+            handler.add_server_config(guild_id)
+            self.create_guild_info(guild_id, handler)
